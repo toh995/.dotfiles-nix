b980e4976b943392e50a48aaf96c741bfe9be6bf
toh995 <52012721+toh995@users.noreply.github.com>
2023-05-28 20:46:16 +0000

    move zsh files to appropriate XDG locations

--- modules/home-manager/zsh/default.nix
+++ modules/home-manager/zsh/default.nix
@@ -1,8 +1,13 @@
+{ config, ... }:
+
 {
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
     enableCompletion = true;
     enableSyntaxHighlighting = true;
+
+    dotDir = ".config/zsh";
+    history.path = "${config.xdg.stateHome}/zsh/.zsh_history";
   };
 }
