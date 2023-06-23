289b056d3dc69920cf4fbe4ff5aa4881d54d5a74
toh995 <52012721+toh995@users.noreply.github.com>
2023-06-22 23:28:05 -0700

    refactor/simplify qtile code

--- modules/home-manager/qtile/config.py
+++ modules/home-manager/qtile/config.py
@@ -3,8 +3,10 @@ from enum import Enum, auto
 from libqtile import hook, qtile
 from libqtile import bar, widget
 from libqtile.config import Group, Key, Screen
+from libqtile.core.manager import Qtile
 from libqtile.lazy import lazy
 
+cursor_warp = True
 
 screens = [
     Screen(
@@ -44,6 +46,24 @@ SUPER = "mod4"
 TAB = "Tab"
 
 
+class Direction(Enum):
+    LEFT = auto()
+    RIGHT = auto()
+
+
+# simple curry decorator
+# adapted from https://stackoverflow.com/a/25078860
+# only supports positional args, not keyword args
+def curry(func):
+    def curried(*args):
+        if len(args) < func.__code__.co_argcount:
+            return lambda *args2: curried(*(args + args2))
+        else:
+            return func(*args)
+
+    return curried
+
+
 # screens and groups
 NUM_SCREENS = 3
 NUM_GROUPS_PER_SCREEN = 3
@@ -52,7 +72,7 @@ groups = [Group(str(i)) for i in range(NUM_SCREENS * NUM_GROUPS_PER_SCREEN)]
 
 
 @hook.subscribe.startup
-def set_default_groups():
+def set_default_groups() -> None:
     for screen in qtile.screens:
         screen.cmd_toggle_group(
             group_name=str(NUM_GROUPS_PER_SCREEN * screen.index),
@@ -61,76 +81,62 @@ def set_default_groups():
         screen.previous_group = None
 
 
-@lazy.function
-def focus_next_group(qtile):
-    curr_group_position = int(qtile.current_screen.group.name) % NUM_GROUPS_PER_SCREEN
-    next_group_position = (curr_group_position + 1) % NUM_GROUPS_PER_SCREEN
+def get_next_group_name(direction: Direction, qtile: Qtile) -> str | None:
+    curr_group_position: int = (
+        int(qtile.current_screen.group.name) % NUM_GROUPS_PER_SCREEN
+    )
+
+    match direction:
+        case Direction.LEFT:
+            next_group_position: int = (curr_group_position - 1) % NUM_GROUPS_PER_SCREEN
+        case Direction.RIGHT:
+            next_group_position: int = (curr_group_position + 1) % NUM_GROUPS_PER_SCREEN
+        case _:
+            return None
+
     next_group_id = next_group_position + (
         qtile.current_screen.index * NUM_GROUPS_PER_SCREEN
     )
-    next_group_name = str(next_group_id)
+    return str(next_group_id)
 
+
+@curry
+def focus_group(direction: Direction, qtile: Qtile) -> None:
+    next_group_name = get_next_group_name(direction, qtile)
     qtile.current_screen.cmd_toggle_group(next_group_name)
 
 
-@lazy.function
-def focus_prev_group(qtile):
-    curr_group_position = int(qtile.current_screen.group.name) % NUM_GROUPS_PER_SCREEN
-    next_group_position = (curr_group_position - 1) % NUM_GROUPS_PER_SCREEN
-    next_group_id = next_group_position + (
-        qtile.current_screen.index * NUM_GROUPS_PER_SCREEN
-    )
-    next_group_name = str(next_group_id)
-
-    qtile.current_screen.cmd_toggle_group(next_group_name)
+@curry
+def move_to_group(direction: Direction, qtile: Qtile) -> None:
+    next_group_name = get_next_group_name(direction, qtile)
+    qtile.current_window.cmd_togroup(next_group_name, switch_group=False)
+    focus_group(direction, qtile)
 
 
-@lazy.function
-def move_to_next_group(qtile):
-    curr_group_position = int(qtile.current_screen.group.name) % NUM_GROUPS_PER_SCREEN
-    next_group_position = (curr_group_position + 1) % NUM_GROUPS_PER_SCREEN
-    next_group_id = next_group_position + (
-        qtile.current_screen.index * NUM_GROUPS_PER_SCREEN
-    )
-    next_group_name = str(next_group_id)
-    qtile.current_window.cmd_togroup(next_group_name, switch_group=True)
-
-
-@lazy.function
-def move_to_prev_group(qtile):
-    curr_group_position = int(qtile.current_screen.group.name) % NUM_GROUPS_PER_SCREEN
-    next_group_position = (curr_group_position - 1) % NUM_GROUPS_PER_SCREEN
-    next_group_id = next_group_position + (
-        qtile.current_screen.index * NUM_GROUPS_PER_SCREEN
-    )
-    next_group_name = str(next_group_id)
-    qtile.current_window.cmd_togroup(next_group_name, switch_group=True)
-
-
-@lazy.function
-def focus_prev_screen(qtile):
-    qtile.cmd_prev_screen()
-    qtile.warp_to_screen()
-
-
-@lazy.function
-def focus_next_screen(qtile):
-    qtile.cmd_next_screen()
-    qtile.warp_to_screen()
-
-
-@lazy.function
-def move_to_prev_screen(qtile):
+def get_next_screen_id(direction: Direction, qtile: Qtile) -> int | None:
     curr_screen_id: int = qtile.current_screen.index
-    qtile.current_window.cmd_toscreen(curr_screen_id - 1)
-    qtile.cmd_prev_screen()
+
+    match direction:
+        case Direction.LEFT:
+            return curr_screen_id - 1 % len(qtile.screens)
+        case Direction.RIGHT:
+            return curr_screen_id + 1 % len(qtile.screens)
+        case _:
+            return None
 
 
-@lazy.function
-def move_to_next_screen(qtile):
-    curr_screen_id: int = qtile.current_screen.index
-    qtile.current_window.cmd_toscreen(curr_screen_id + 1)
-    qtile.cmd_next_screen()
+@curry
+def focus_screen(direction: Direction, qtile: Qtile) -> None:
+    next_screen_id = get_next_screen_id(direction, qtile)
+    qtile.cmd_to_screen(next_screen_id)
+    qtile.current_screen.window.focus(warp=True)
+
+
+@curry
+def move_to_screen(direction: Direction, qtile: Qtile) -> None:
+    next_screen_id = get_next_screen_id(direction, qtile)
+    qtile.current_window.cmd_toscreen(next_screen_id)
+    focus_screen(direction, qtile)
 
 
 keys = [
@@ -157,61 +163,28 @@ keys = [
     Key([ALT, SUPER, SHIFT], "j", lazy.layout.shuffle_down()),
     Key([ALT, SUPER, SHIFT], "k", lazy.layout.shuffle_up()),
     # Change focus to adjacent screen
-    # Key([ALT], "h", lazy.prev_screen()),
-    Key([ALT], "h", focus_prev_screen),
-    Key([ALT], "l", focus_next_screen),
+    Key([ALT], "h", lazy.function(focus_screen(Direction.LEFT))),
+    Key([ALT], "l", lazy.function(focus_screen(Direction.RIGHT))),
     # Move current window adjacent screen
     Key(
         [ALT, SHIFT],
         "h",
-        move_to_prev_screen,
+        lazy.function(move_to_screen(Direction.LEFT)),
     ),
     Key(
         [ALT, SHIFT],
         "l",
-        move_to_next_screen,
+        lazy.function(move_to_screen(Direction.RIGHT)),
     ),
     # Change focus to the next group (on the same screen)
-    Key([SUPER], "h", focus_prev_group),
-    Key([SUPER], "l", focus_next_group),
+    Key([SUPER], "h", lazy.function(focus_group(Direction.LEFT))),
+    Key([SUPER], "l", lazy.function(focus_group(Direction.RIGHT))),
     # Move current window to the next group (on the same screen)
-    Key([SUPER, SHIFT], "h", move_to_prev_group),
-    Key([SUPER, SHIFT], "l", move_to_next_group),
+    Key([SUPER, SHIFT], "h", lazy.function(move_to_group(Direction.LEFT))),
+    Key([SUPER, SHIFT], "l", lazy.function(move_to_group(Direction.RIGHT))),
     # Change focus to the last-used group on the current screen
     Key([ALT], TAB, lazy.screen.toggle_group()),
     # Reload
     Key([SUPER, CONTROL], "r", lazy.reload_config(), desc="Reload the config"),
     Key([SUPER, CONTROL], "q", lazy.shutdown(), desc="Shutdown Qtile"),
 ]
-
-
-# Directions
-class Direction(Enum):
-    LEFT = auto()
-    RIGHT = auto()
-
-
-#
-#
-# @lazy.function
-# def change_screen(qtile, direction: Direction):
-#     # logger.warning("HELLO")
-#     # curr_screen_id: int = qtile.current_screen.index
-#     curr_screen_id: int = qtile.current_screen.index
-#
-#     # if direction == Direction.LEFT:
-#     #     new_screen_id = curr_screen_id - 1
-#     # elif direction == Direction.RIGHT:
-#     #     new_screen_id = curr_screen_id + 1
-#     if direction == Direction.LEFT:
-#         new_screen_id = curr_screen_id - 1
-#     elif direction == Direction.RIGHT:
-#         new_screen_id = curr_screen_id + 1
-#
-#     logger.warning(f"new_screen_id {new_screen_id}")
-#     # logger.warning(curr_window)
-#
-#     # lazy.window.toscreen(new_screen_id)()
-#     qtile.current_window.cmd_toscreen(new_screen_id)
-#     qtile.cmd_prev_screen(new_screen_id)
-#     # qtile.current_window.cmd_focus()
