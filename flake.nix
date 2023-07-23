96df15837bfad0ea4fb032628827f6bfd9b72cae
toh995 <52012721+toh995@users.noreply.github.com>
2023-07-23 15:16:50 -0700

    update flake.lock, and add NUR

--- flake.nix
+++ flake.nix
@@ -2,17 +2,20 @@
   description = "NixOS Config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nur.url = "github:nix-community/NUR";
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
+  outputs = { home-manager, nixpkgs, nur, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        nur.nixosModules.nur
+        { nixpkgs.overlays = [nur.overlay]; }
         ./modules/hardware-config.nix
         ./modules/system-config.nix
         home-manager.nixosModules.home-manager
