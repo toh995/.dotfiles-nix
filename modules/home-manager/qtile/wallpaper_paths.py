02ed8f90a330dfeaf24d4ab2957508318a066704
toh995 <52012721+toh995@users.noreply.github.com>
2024-02-24 12:03:26 -0800

    add pikachu reddit wallpaper

--- modules/home-manager/qtile/wallpaper_paths.py
+++ modules/home-manager/qtile/wallpaper_paths.py
@@ -10,4 +10,5 @@ WALLPAPER_DIR = os.path.join(
 COLORFUL_ASTRONAUT = os.path.join(WALLPAPER_DIR, "colorful-astronaut.jpg")
 MILES_MORALES = os.path.join(WALLPAPER_DIR, "miles-morales.jpg")
 STAR_WARS_ANIME = os.path.join(WALLPAPER_DIR, "star-wars-anime.jpg")
+PIKACHU_REDDIT = os.path.join(WALLPAPER_DIR, "pikachu-reddit.png")
 TOKYO_EVENING_ROAD = os.path.join(WALLPAPER_DIR, "tokyo-evening-road.jpg")
