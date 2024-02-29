4af606abbed419b83a5e80f8f515466ff94b527c
toh995 <52012721+toh995@users.noreply.github.com>
2024-02-28 20:38:07 -0800

    tmux catppuccin theme

--- modules/home-manager/tmux/default.nix
+++ modules/home-manager/tmux/default.nix
@@ -1,3 +1,9 @@
 {
-  xdg.configFile."tmux/tmux.conf".source = ./tmux.conf;
+  xdg.configFile = {
+    "tmux/tmux.conf".source = ./tmux.conf;
+    "tmux/catppuccin".source = builtins.fetchGit {
+      url = "https://github.com/catppuccin/tmux.git";
+      rev = "a0119d25283ba2b18287447c1f86720a255fb652"; # commit SHA
+    };
+  };
 }
