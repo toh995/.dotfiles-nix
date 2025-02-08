9108aef5960451b8837cf3f82d3f58bfa040176f
toh995 <52012721+toh995@users.noreply.github.com>
2025-02-08 14:52:38 -0800

    add bluetooth support

--- modules/system-config.nix
+++ modules/system-config.nix
@@ -78,6 +78,11 @@
     jack.enable = true;
   };
 
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
   # fonts
   fonts = {
     packages = with pkgs; [
