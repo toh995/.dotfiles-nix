10318f1a982e76caba9efd8eb1a172534591c1f6
toh995 <52012721+toh995@users.noreply.github.com>
2026-08-16 22:57:13 -0700

    set the firefox config location to silence warning

--- modules/home-manager/firefox/default.nix
+++ modules/home-manager/firefox/default.nix
@@ -1,5 +1,18 @@
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   programs.firefox.enable = true;
+
+  # If home.stateVersion < 26.05 (like we are), then config will be saved to
+  # `~/.mozilla/firefox`. This causes a warning during nixos reubuilds.
+  # For later state versions, we use the XDG config by default
+  # (i.e. `~/.config/mozilla/firefox`).
+  # We can silence the warning, by explicitly opting in to the new config file location.
+  programs.firefox.configPath = "${lib.removePrefix "${config.home.homeDirectory}/" config.xdg.configHome}/mozilla/firefox";
+
   programs.firefox.profiles.toh995 = {
     isDefault = true;
     id = 0;
